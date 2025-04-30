a87ae922a2754fe1afe96e60f761180ade745baa
vidhyamtech <vidhya.mtech@gmail.com>
2025-04-30 11:50:24 -0500

    Add files via upload

---
+++ Day6/Vidhya_Assignment_Day6.sql
@@ -0,0 +1,13 @@
+-----------Q1------------
+SELECT product_name,CASE WHEN units_in_stock = 0 THEN 'Out of Stock' WHEN units_in_stock < 20 THEN 'Low Stock'
+ELSE 'Available' END AS stock_status FROM products ORDER BY stock_status;
+ 
+-----------Q2------------
+SELECT product_name, unit_price FROM products WHERE category_id = (SELECT category_id FROM categories	WHERE category_name = 'Beverages');
+ 
+-----------Q3------------
+SELECT order_id, order_date, freight, employee_id FROM orders WHERE employee_id = (SELECT employee_id FROM orders GROUP BY employee_id ORDER BY COUNT (*) DESC LIMIT 1);
+ 
+-----------Q4------------
+SELECT order_id, order_date, freight, ship_country FROM orders WHERE ship_country != 'USA' AND freight > ALL (SELECT freight  FROM orders  WHERE ship_country = 'USA');
+SELECT order_id, order_date, freight, ship_country FROM orders WHERE ship_country != 'USA' AND freight > ANY (SELECT freight  FROM orders  WHERE ship_country = 'USA');
