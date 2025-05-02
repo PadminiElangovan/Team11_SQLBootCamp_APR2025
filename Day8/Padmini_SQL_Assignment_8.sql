59154b38ac7d02ca86952cbcd8353cbba7059a87
PadminiElangovan <padmini1991@gmail.com>
2025-05-02 00:57:20 -0500

    Create Padmini_SQL_Assignment_8.sql

---
+++ Day8/Padmini_SQL_Assignment_8.sql
@@ -0,0 +1,70 @@
+----------------- Day 8 -----------------------
+ 
+-- 1. Create view vw_updatable_products (use same query whatever I used in the training)
+/*Try updating view with below query and see if the product table also gets updated.
+Update query: UPDATE updatable_products SET unit_price = unit_price * 1.1 WHERE units_in_stock < 10; */
+SELECT * FROM products;
+SELECT * FROM vw_updatable_products;
+
+CREATE VIEW vw_updatable_products AS 
+SELECT product_id, product_name, unit_price, units_in_stock, discontinued
+FROM products 
+WHERE discontinued = 0
+WITH CHECK OPTION
+
+UPDATE vw_updatable_products SET unit_price = unit_price * 1.1 WHERE units_in_stock < 10;
+ 
+-- 2. Transaction:
+/* Update the product price for products by 10% in category id=1
+Try COMMIT and ROLLBACK and observe what happens. */
+SELECT * FROM products;
+SELECT * FROM products WHERE category_id = 1;
+
+BEGIN;
+
+UPDATE products
+SET unit_price = unit_price * 1.10
+WHERE category_id = 1;
+
+ROLLBACK;
+
+COMMIT;
+ 
+-- 3. Create a regular view which will have below details (Need to do joins):
+/* Employee_id, Employee_full_name, Title, Territory_id, territory_description, region_description */
+SELECT * FROM employees;
+SELECT * FROM territories;
+SELECT * FROM region;
+
+CREATE VIEW vw_employee_territory AS
+SELECT e.employee_id,
+	e.first_name || ' ' || e.last_name AS employee_full_name, e.title,
+    t.territory_id, t.territory_description,
+    r.region_description
+FROM employees e
+JOIN employee_territories et ON e.employee_id = et.employee_id
+JOIN territories t ON et.territory_id = t.territory_id
+JOIN region r ON t.region_id = r.region_id;
+
+ SELECT * FROM vw_employee_territory;
+ 
+-- 4. Create a recursive CTE based on Employee Hierarchy
+ SELECT employee_id, 
+ first_name || ' ' || last_name AS employee_name, reports_to FROM employees;
+
+WITH RECURSIVE cte_employee_hierarchy AS (
+SELECT employee_id, first_name, last_name, reports_to, 0 AS level
+FROM  employees e
+WHERE reports_to IS NULL
+
+UNION ALL
+
+SELECT e.employee_id, e.first_name, e.last_name, e.reports_to, eh.level+1
+FROM employees e
+JOIN cte_employee_hierarchy eh
+ON eh.employee_id = e.reports_to )
+
+SELECT level, employee_id, 
+first_name || ' ' || last_name AS employee_full_name
+FROM cte_employee_hierarchy
+ORDER BY level, employee_id;
