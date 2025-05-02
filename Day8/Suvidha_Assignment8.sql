d91db137dde50b3f4729c024bdb9d0a0152d95d5
Suvidha-Rai <suvidha.rai@gmail.com>
2025-05-02 12:35:07 -0400

    Add files via upload

---
+++ Day8/Suvidha_Assignment8.sql
@@ -0,0 +1,54 @@
+/* 1.     Create view vw_updatable_products (use same query whatever I used in the training)
+Try updating view with below query and see if the product table also gets updated.
+Update query:
+UPDATE updatable_products SET unit_price = unit_price * 1.1 WHERE units_in_stock < 10;*/
+
+create or replace view vw_updatable_products
+as 
+select product_id,product_name,unit_price,
+units_in_stock,discontinued
+from products
+where discontinued=0
+with check option
+
+update vw_updatable_products
+set unit_price=unit_price * 1.1
+where units_in_stock<10;
+
+select * from vw_updatable_products
+
+
+/* 2.Transaction:
+Update the product price for products by 10% in category id=1
+Try COMMIT and ROLLBACK and observe what happens.*/
+
+begin;
+update products
+set unit_price=unit_price * 1.1
+where category_id=1;
+
+commit;
+
+rollback;
+
+select * from products
+where category_id=1;
+
+/* 3.     Create a regular view which will have below details (Need to do joins):
+Employee_id,
+Employee_full_name,
+Title,
+Territory_id,
+territory_description,
+region_description */
+
+create or replace view vw_emp_details as 
+select e.employee_id, concat(first_name,' ',last_name) as employee_name,
+title,et.territory_id,territory_description,region_description
+from employees e
+inner join employee_territories et using(employee_id)
+inner join territories t using(territory_id)
+inner join region r using(region_id)
+
+select * from vw_emp_details
+
